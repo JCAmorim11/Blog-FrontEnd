b8504ba72ea9868c80732665d56099a80e32b590
JCAmorim23 <juliana.carol.amorim@gmail.com>
2022-10-14 19:39:17 -0300

    aula 3

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent,
+    FeedComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
