c97f02a7daa1f3521a3d4952fd516ecb064776a8
JCAmorim23 <juliana.carol.amorim@gmail.com>
2022-10-14 21:21:53 -0300

    aula 4

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FeedComponent } from './feed/feed.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
