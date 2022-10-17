a7db04c42e01edee40ba05ad5b41ae1e7a37f189
JCAmorim23 <juliana.carol.amorim@gmail.com>
2022-10-17 00:35:36 -0300

    inicio aula 5

--- front/src/app/footer/footer.component.ts
+++ front/src/app/footer/footer.component.ts
@@ -1,4 +1,7 @@
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { Component, OnInit } from '@angular/core';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  
+
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
+  faLinkedin = faLinkedin;
 
   constructor() { }
 
